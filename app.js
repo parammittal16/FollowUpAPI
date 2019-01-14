004f29bb74308fa30f2da9f9ddba7a4cccdf26be
rishabhscross <parammittal16@gmail.com>
2019-01-14 15:09:31 +0530

    Add and configure sequelize with heroku

--- app.js
+++ app.js
@@ -1,22 +1,51 @@
 const express = require('express');
 const app = express();
 const port=process.env.PORT || 3000;
-const { Client } = require('pg');
+// const { Client } = require('pg');
+const sequelize = require('./utils/database');
+const Product = require('./models/product');
+var pg = require('pg');
+pg.defaults.ssl = true;
 
-const dbConnectionURL = 'postgres://vehffbqamvhnci:e9ad7bb1267dfe6a9f62fb2c11a9b04a6efe1ba99df8d6b5101d785c7c92ad0c@ec2-23-21-171-25.compute-1.amazonaws.com:5432/dd0i4vp4b58dq1';
-const client = new Client({
-  connectionString: dbConnectionURL,
-  ssl: true,
-});
-client.connect();
+// const client = new Client({
+//   user: 'vehffbqamvhnci',
+//   host: 'ec2-23-21-171-25.compute-1.amazonaws.com',
+//   database: 'dd0i4vp4b58dq1',
+//   password: 'e9ad7bb1267dfe6a9f62fb2c11a9b04a6efe1ba99df8d6b5101d785c7c92ad0c',
+//   port: 5432,
+//   ssl: true
+// })
+
+//const dbConnectionURL = 'postgres://vehffbqamvhnci:e9ad7bb1267dfe6a9f62fb2c11a9b04a6efe1ba99df8d6b5101d785c7c92ad0c@ec2-23-21-171-25.compute-1.amazonaws.com:5432/dd0i4vp4b58dq1';
+// const client = new Client({
+//   connectionString: dbConnectionURL,
+//   ssl: true,
+// });
+// client.connect();
+// app.get('/', function (req, res) {
+//   client.query("SELECT * FROM link;", (err, result) => {
+//     if (err) throw err;
+//     res.send(result.rows);
+//   });
+// });
 app.get('/', function (req, res) {
-  client.query("SELECT * FROM link;", (err, result) => {
-    if (err) throw err;
-    res.send(result.rows);
-  });
+  Product.create({
+    title: 'sds',
+    price: 45.2
+  })
+  .then(result => {
+    console.log(result);
+    res.send(result);
+  })
+  .catch(err => console.log(err))
 });
 
-
-app.listen(port, function () {
-  console.log('Example app listening on port ' + port);
-});
+sequelize
+  .sync()
+  .then(res => {
+    console.log(res);
+    app.listen(port, function () {
+      console.log('Example app listening on port ' + port);
+    })
+  })
+  .catch(err => console.log(err));
---
+++ models/product.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../utils/database');
+
+const Product = sequelize.define('product', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    title: Sequelize.STRING,
+    price: {
+        type: Sequelize.DOUBLE
+    }
+});
+
+module.exports = Product;
