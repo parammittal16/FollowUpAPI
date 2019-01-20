476c147131350f610ee9ebaf2a57dc34e69f5a35
PARAM MITTAL <parammittal16@gmail.com>
2019-01-20 19:49:37 +0530

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -1,6 +1,5 @@
 const Sequelize = require('sequelize');
-let url = process.env.DATABASE_URL || "postgres://vehffbqamvhnci:e9ad7bb1267dfe6a9f62fb2c11a9b04a6efe1ba99df8d6b5101d785c7c92ad0c@ec2-23-21-171-25.compute-1.amazonaws.com:5432/dd0i4vp4b58dq1"; 
-
+let url = process.env.DATABASE_URL;
 const sequelize = new Sequelize(url, {
     dialect: 'postgres',
     protocol: 'postgres',
